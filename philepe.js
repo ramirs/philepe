c1a270a8397690a559cea98896c2faab2d789a6d
ramirs <ramirs2015@gmail.com>
2017-05-31 20:28:40 -0700

    removing uneccessary init function, updating targetDims object to a global so it can be updated outside of appendBarChart()

--- philepe.js
+++ philepe.js
@@ -1,21 +1,26 @@
-function Philepe () {}
+function Philepe () {
+  this.targetDims = { //container client dims
+    maxH: 0,
+    maxW: 0
+  };
+}
 
-Philepe.prototype.init = function () {};
-
-//getData() { returns data array, one element for each piece of data captured by Philepe (every time we init a new visual, push {container: data} to array) }
+Philepe.prototype.updateTargetDims = function (dims) {
+  this.targetDims = {
+    maxH: dims.clientHeight,
+    maxW: dims.clientWidth
+  }
+}
 
 Philepe.prototype.appendBarChart = function (anchor, data) {
   let dataToRender = data;
+  this.updateTargetDims(anchor);
   console.log(dataToRender);
-  let targetDims = { //container client dims
-    maxH: anchor.clientHeight,
-    maxW: anchor.clientWidth
-  };
 
   //set container
   let barContainer = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  barContainer.setAttribute("height", "" + targetDims.maxH);
-  barContainer.setAttribute("width", "" + targetDims.maxW);
+  barContainer.setAttribute("height", "" + this.targetDims.maxH);
+  barContainer.setAttribute("width", "" + this.targetDims.maxW);
   //attributes can also set style
 
   //render axis (if user allows)
@@ -23,16 +28,16 @@ Philepe.prototype.appendBarChart = function (anchor, data) {
     //y axis
     let yAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
     yAxisElement.setAttribute("y1", "" + 0);
-    yAxisElement.setAttribute("y2", "" + targetDims.maxH);
+    yAxisElement.setAttribute("y2", "" + this.targetDims.maxH);
     yAxisElement.setAttribute("stroke-width", "3");
     yAxisElement.setAttribute("stroke", "#7f8c8d");
     barContainer.appendChild(yAxisElement);
     //x axis
     let xAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
     xAxisElement.setAttribute("x1", "" + 0);
-    xAxisElement.setAttribute("x2", "" + targetDims.maxW);
-    xAxisElement.setAttribute("y1", "" + targetDims.maxH);
-    xAxisElement.setAttribute("y2", "" + targetDims.maxH);
+    xAxisElement.setAttribute("x2", "" + this.targetDims.maxW);
+    xAxisElement.setAttribute("y1", "" + this.targetDims.maxH);
+    xAxisElement.setAttribute("y2", "" + this.targetDims.maxH);
     xAxisElement.setAttribute("stroke-width", "3");
     xAxisElement.setAttribute("stroke", "#7f8c8d");
     barContainer.appendChild(xAxisElement);
@@ -40,7 +45,7 @@ Philepe.prototype.appendBarChart = function (anchor, data) {
 
   //begin to parse data attributes
   let elemCount = dataToRender.data.length;
-  let elemW = targetDims.maxW / ((elemCount * 2) + 1);
+  let elemW = this.targetDims.maxW / ((elemCount * 2) + 1);
   let elemMaxValue = 0; //capture initial max, if any
   if(typeof data.targetData !== 'undefined' && data.targetData != null) {
     elemMaxValue = data.targetData;
@@ -60,21 +65,22 @@ Philepe.prototype.appendBarChart = function (anchor, data) {
   //create all bars and append them to container
   for (var j = 0; j <= dataToRender.data.length - 1; j++) {
     let barElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    barElement.setAttribute("height", "" + (dataToRender.data[j] / elemMaxValue) * targetDims.maxH);
+    barElement.setAttribute("height", "" + (dataToRender.data[j] / elemMaxValue) * this.targetDims.maxH);
     barElement.setAttribute("width", "" + elemW);
     barElement.setAttribute("x", "" + positionLeft * elemW);
-    barElement.setAttribute("y", "" + ((elemMaxValue - dataToRender.data[j]) / elemMaxValue * targetDims.maxH ));
+    barElement.setAttribute("y", "" + ((elemMaxValue - dataToRender.data[j]) / elemMaxValue * this.targetDims.maxH ));
     barElement.setAttribute("fill", "#2c3e50")
     barContainer.appendChild(barElement);
     positionLeft = positionLeft + 2;//accounts for padding between bar, linearly spaced
   }
+
   //optional user input : a line to show a 'minimum' height for any bar to pass
   if(typeof data.minData !== 'undefined' && data.minData != null) {
     let minLineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
     minLineElement.setAttribute("stroke-dasharray", "5,5");
     minLineElement.setAttribute("x1", "0");
-    minLineElement.setAttribute("x2", "" + targetDims.maxW);
-    let minLineHeight = (elemMaxValue - data.minData) / elemMaxValue * targetDims.maxH;
+    minLineElement.setAttribute("x2", "" + this.targetDims.maxW);
+    let minLineHeight = (elemMaxValue - data.minData) / elemMaxValue * this.targetDims.maxH;
     minLineElement.setAttribute("y1", "" + minLineHeight);
     minLineElement.setAttribute("y2", "" + minLineHeight);
     minLineElement.setAttribute("stroke", "#c0392b");
@@ -85,14 +91,18 @@ Philepe.prototype.appendBarChart = function (anchor, data) {
     let maxLineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
     maxLineElement.setAttribute("stroke-dasharray", "5,5");
     maxLineElement.setAttribute("x1", "0");
-    maxLineElement.setAttribute("x2", "" + targetDims.maxW);
-    let maxLineHeight = (elemMaxValue - data.targetData) / elemMaxValue * targetDims.maxH;
+    maxLineElement.setAttribute("x2", "" + this.targetDims.maxW);
+    let maxLineHeight = (elemMaxValue - data.targetData) / elemMaxValue * this.targetDims.maxH;
     maxLineElement.setAttribute("y1", "" + maxLineHeight);
     maxLineElement.setAttribute("y2", "" + maxLineHeight);
     maxLineElement.setAttribute("stroke", "#16a085");
     barContainer.appendChild(maxLineElement);
   }
 
+  //optional user input : ticks to
+
   //append container, which has at least all bars and any optional user inputs attached
   anchor.appendChild(barContainer);
 };
+
+//TODO: getData() { returns data array, one element for each piece of data captured by Philepe (every time we init a new visual, push {container: data} to array) }
