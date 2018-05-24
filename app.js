e6b83a10c831ed56f72082d22e2cad72498ac80a
ramirs <ramirs2015@gmail.com>
2018-05-23 22:58:29 -0700

    removing garbage code, switching logic to inheritance pattern so we can easily build out new graphs

--- app.js
+++ app.js
@@ -1,53 +1,9 @@
 (function() {
 
-  let philepe = new Philepe();
   let container = document.getElementById("bar-chart-container");
-  let lastRand;
 
-  let arrayOfRand = function (n) {
-    let maxNum = document.getElementById('input-max-element').value;
-    let arr = new Array();
-    for (let i = 0; i < n; i++) {
-      arr.push(Math.ceil(Math.random() * maxNum));
-    }
-    lastRand = arr;
-    return lastRand;
-  };
-
-  let createBarChart = function () {
-    philepe.appendBarChart(container, {
-      data: typeof lastRand === 'undefined'? arrayOfRand(document.getElementById('input-elements').value) : lastRand,
-      minData: document.getElementById('input-min').value,
-      targetData: document.getElementById('input-max').value,
-      axis: true,
-
-      // minData: 1, //commenting this back will add the 'minData' line to the chart
-      // targetData: 5, //commenting this back will add the 'targetData' line to the chart
-      // axis: false, //commenting this back will remove the axis on both sides
-      // ticks: 10, //TODO: scaled to maxValueHeight (maxValue = 5, ticks = 10, step every 0.5 || maxValue = 10, ticks = 4, step every 2.5)
-      // tickOrientation: null, //TODO: ?? origin: start tick value at 0, excluding max value || center: exclude 0 and max value from tick positions || maxFirst: exclude 0 from tick values || null: default (inclusive of 0 and max value)
-      // colors: {bars:'', axis:'', ticks:'', labels:''}, //TODO: custom styles for objects
-      // orientation: null, //TODO: horizontal or vertical
-      // negativeValues: true //TODO: adjust axis height and bar placement
-
-    });
-  };
-
-  let repaintBars = function () {
-    container.innerHTML = '';
-    createBarChart();
-  };
-  
-  let updateData = function () {
-    arrayOfRand(document.getElementById('input-elements').value);
-    repaintBars();
-  };
-
-  document.getElementById('input-elements').addEventListener('change', updateData);
-  document.getElementById('input-max-element').addEventListener('change', updateData);
-  document.getElementById('input-min').addEventListener('change', repaintBars);
-  document.getElementById('input-max').addEventListener('change', repaintBars);
-    
-  createBarChart();
+  new BarChart(container, new MockData(25, 'Linear'));
+  new LineChart(container, new MockData(25, 'Linear'));
+  new ScatterPlot(container, new MockData(25, 'Linear'));
 
 })();
--- philepe.js
+++ philepe.js
@@ -1,156 +1,204 @@
 
-function Philepe () {
-  this.targetDims = { //container client dims
-    maxH: 0,
-    maxW: 0
-  };
-}
+/**
+ * PHILEPE.JS
+ * 
+ * A lightweight data visualization library
+ * 
+ */
 
-Philepe.prototype.updateTargetDims = function (dims) {
-  this.targetDims = {
-    maxH: dims.clientHeight,
-    maxW: dims.clientWidth
+
+
+/**
+ * Theme config for styling
+ */
+const Colors = {
+  axis: "#7f8c8d",
+  bars: "#2c3e50",
+  dots: "#2c3e50"
+}
+/**
+ * Root object for any Philepe chart
+ */
+class Philepe {
+  /**
+   * @param {HTMLElement} container renders the axis lines for any chart
+   */
+  constructor(container){
+    this.dims = { h: container.clientHeight, w: container.clientWidth };
+    this.boundary = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+    this.boundary.setAttribute("height", this.dims.h);
+    this.boundary.setAttribute("width", this.dims.w);
+
+    this.boundary.appendChild(this.renderYAxis());
+    this.boundary.appendChild(this.renderXAxis());
+
+    container.appendChild(this.boundary);
   }
-}
-
-Philepe.prototype.renderYAxis = function () {
-
-  let yAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  yAxisElement.setAttribute("y1", "" + 0);
-  yAxisElement.setAttribute("y2", "" + this.targetDims.maxH);
-  yAxisElement.setAttribute("stroke-width", "3");
-  yAxisElement.setAttribute("stroke", "#7f8c8d");
-
-  return yAxisElement;
-}
-
-Philepe.prototype.renderYAxisTicks = function (anchor, ticks, maxY) {
-  //TODO: render ticks
-
-  // console.log(ticks);
-  // console.log(maxY);
-
-  for(let i = 0; i <= ticks + 1; i++) {
-    let yAxisTick = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    yAxisTick.setAttribute("stroke-width", "1");
-    yAxisTick.setAttribute("stroke", "#95a5a6");
-    yAxisTick.setAttribute("x1", "" + 0);
-    yAxisTick.setAttribute("x2", "" + this.targetDims.maxW);
-    yAxisTick.setAttribute("y1", this.targetDims.maxH / (ticks + 1) * i);
-    yAxisTick.setAttribute("y2", this.targetDims.maxH / (ticks + 1) * i);
-
-    // console.log(maxY / (ticks + 1) * i);
-    // console.log(yAxisTick);
-    anchor.appendChild(yAxisTick);
-    //
-    // let yTickLabel = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    // yTickLabel.textContent = "" + Math.floor(maxY / (ticks + 1) * i);
-    // yTickLabel.setAttribute("fill", "#7f8c8d");
-    // yTickLabel.setAttribute("x", "-10");
-    // yTickLabel.setAttribute("y", this.targetDims.maxH / (ticks + 1) * i);
-    // // 0 + (vertical share)
-    // // console.log(yTickLabel);
-    // anchor.appendChild(yTickLabel);
+  /**
+   * creates and returns an element that outlines the left side of the container
+   */
+  renderYAxis () {
+    let yAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    yAxisElement.setAttribute("y1", 0);
+    yAxisElement.setAttribute("y2", this.dims.h);
+    yAxisElement.setAttribute("stroke-width", "3");
+    yAxisElement.setAttribute("stroke", Colors.axis);
+    return yAxisElement;
   }
-}
-
-Philepe.prototype.renderXAxis = function (ticks) {
-  //TODO: render ticks
-  // console.log(typeof ticks !== 'undefined')
-
-  let xAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
-  xAxisElement.setAttribute("x1", "" + -10);
-  xAxisElement.setAttribute("x2", "" + this.targetDims.maxW);
-  xAxisElement.setAttribute("y1", "" + this.targetDims.maxH);
-  xAxisElement.setAttribute("y2", "" + this.targetDims.maxH);
-  xAxisElement.setAttribute("stroke-width", "3");
-  xAxisElement.setAttribute("stroke", "#7f8c8d");
-  return xAxisElement;
-}
-
-Philepe.prototype.appendBarChart = function (anchor, data) {
-  let dataToRender = data;
-  this.updateTargetDims(anchor);
-  console.log(dataToRender);
-
-  //set container
-  let barContainer = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  barContainer.setAttribute("height", "" + this.targetDims.maxH);
-  barContainer.setAttribute("width", "" + this.targetDims.maxW);
-  // barContainer.setAttribute("style", "padding: 20px;");
-  //attributes can also set style
-
-  //begin to parse data attributes
-  let elemCount = dataToRender.data.length;
-  let elemW = this.targetDims.maxW / ((elemCount * 2) + 1);
-  let elemMaxValue = 0; //capture initial max, if any
-  if(typeof data.targetData !== 'undefined' && data.targetData != null) {
-    elemMaxValue = data.targetData;
+  /**
+   * creates and returns an element that outlines the bottom side of the container
+   */
+  renderXAxis () {
+    let xAxisElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    xAxisElement.setAttribute("x1", -10);
+    xAxisElement.setAttribute("x2", this.dims.w);
+    xAxisElement.setAttribute("y1", this.dims.h);
+    xAxisElement.setAttribute("y2", this.dims.h);
+    xAxisElement.setAttribute("stroke-width", "3");
+    xAxisElement.setAttribute("stroke", Colors.axis);
+    return xAxisElement;
   }
-  if(typeof data.minData !== 'undefined' && data.minData != null) {
-    if(data.minData > elemMaxValue) {
-      elemMaxValue = data.minData;
+};
+/**
+ * Bar Chart object
+ */
+class BarChart extends Philepe {
+  constructor(container, data) {
+    super(container);
+
+    this.render(data);
+  }
+
+  /**
+   * Creates a series of bars that visualize data d, appending them to the root graph
+   * @param {Array} d any array of integers
+   */
+  render (d) {
+    //begin to parse data attributes
+    let elemCount = d.length;
+    let elemW = this.dims.w / ((elemCount * 2) + 1);
+    let elemMaxValue = 0;
+    //first loop, check to 'finalize' maxValue in data
+    for (var i = 0; i < d.length; i++) {
+      if(d[i] > elemMaxValue) {
+        elemMaxValue = d[i];
+      }
+    }
+    let positionLeft = 1;
+    //create all bars and append them to container
+    for (var j = 0; j <= d.length - 1; j++) {
+      let barElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+      barElement.setAttribute("height", (d[j] / elemMaxValue) * this.dims.h);
+      barElement.setAttribute("width", elemW);
+      barElement.setAttribute("x", positionLeft * elemW);
+      barElement.setAttribute("y", ((elemMaxValue - d[j]) / elemMaxValue * this.dims.h ));
+      barElement.setAttribute("fill", Colors.bars)
+      this.boundary.appendChild(barElement);
+      positionLeft = positionLeft + 2;//accounts for padding between bar
     }
   }
-  //first loop, check to 'finalize' maxValue in data
-  for (var i = 0; i < dataToRender.data.length; i++) {
-    if(dataToRender.data[i] > elemMaxValue) {
-      elemMaxValue = dataToRender.data[i];
+};
+/**
+ * ScatterPlot object
+ */
+class ScatterPlot extends Philepe {
+  constructor(container, data) {
+    super(container);
+
+    this.render(data);
+  }
+
+  render (d) {
+    //begin to parse data attributes
+    let elemCount = d.length;
+    let elemLoc = this.dims.w / ((elemCount * 2) + 1);
+    let elemMaxValue = 0;
+    //first loop, check to 'finalize' maxValue in data
+    for (var i = 0; i < d.length; i++) {
+      if(d[i] > elemMaxValue) {
+        elemMaxValue = d[i]; //account for padding (radius)
+      }
+    }
+    let positionLeft = 1;
+    //create all bars and append them to container
+    for (var j = 0; j <= d.length - 1; j++) {
+      let el = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+      el.setAttribute("cx", positionLeft * elemLoc);
+      el.setAttribute("cy", ((elemMaxValue - d[j]) / elemMaxValue * this.dims.h ));
+      el.setAttribute("r", "3");
+      el.setAttribute("fill", Colors.bars)
+      this.boundary.appendChild(el);
+      positionLeft = positionLeft + 2;//accounts for padding between bar
     }
   }
-  let positionLeft = 1;
+};
+/**
+ * Line Chart object
+ */
+class LineChart extends Philepe {
+  constructor(container, data) {
+    super(container);
 
-  //create all bars and append them to container
-  for (var j = 0; j <= dataToRender.data.length - 1; j++) {
-    // if(typeof elemMaxValue === 'undefined') { elemMaxValue = 0 }
-    let barElement = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-    barElement.setAttribute("height", "" + (dataToRender.data[j] / elemMaxValue) * this.targetDims.maxH);
-    barElement.setAttribute("width", "" + elemW);
-    barElement.setAttribute("x", "" + positionLeft * elemW);
-    barElement.setAttribute("y", "" + ((elemMaxValue - dataToRender.data[j]) / elemMaxValue * this.targetDims.maxH ));
-    barElement.setAttribute("fill", "#2c3e50")
-    barContainer.appendChild(barElement);
-    positionLeft = positionLeft + 2;//accounts for padding between bar, linearly spaced
+    console.log(data);
+    this.render(data);
   }
 
-  //optional user input : a line to show a 'minimum' height for any bar to pass
-  if(typeof data.minData !== 'undefined' && data.minData != null) {
-    let minLineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    minLineElement.setAttribute("stroke-dasharray", "5,5");
-    minLineElement.setAttribute("x1", "0");
-    minLineElement.setAttribute("x2", "" + this.targetDims.maxW);
-    let minLineHeight = (elemMaxValue - data.minData) / elemMaxValue * this.targetDims.maxH;
-    minLineElement.setAttribute("y1", "" + minLineHeight);
-    minLineElement.setAttribute("y2", "" + minLineHeight);
-    minLineElement.setAttribute("stroke", "#c0392b");
-    barContainer.appendChild(minLineElement);
+  render (d) {
+    //begin to parse data attributes
+    let elemCount = d.length;
+    let elemLoc = this.dims.w / ((elemCount * 2) + 1);
+    let elemMaxValue = 0;
+    //first loop, check to 'finalize' maxValue in data
+    for (var i = 0; i < d.length; i++) {
+      if(d[i] > elemMaxValue) {
+        elemMaxValue = d[i]; //account for padding (radius)
+      }
+    }
+    let positionLeft = 1;
+    //create all bars and append them to container (start at 2nd element)
+    for (var j = 1; j <= d.length - 1; j++) {
+      let el = document.createElementNS("http://www.w3.org/2000/svg", "line");
+      el.setAttribute("x1", positionLeft * elemLoc);
+      el.setAttribute("x2", (positionLeft + 2) * elemLoc);
+      el.setAttribute("y1", ((elemMaxValue - d[j - 1]) / elemMaxValue * this.dims.h ));
+      el.setAttribute("y2", ((elemMaxValue - d[j]) / elemMaxValue * this.dims.h ));
+      el.setAttribute("stroke", Colors.bars)
+      this.boundary.appendChild(el);
+      positionLeft = positionLeft + 2;//accounts for padding between bar
+    }
   }
-  //optional user input : a line to show a 'goal' height for any bar to pass
-  if(typeof data.targetData !== 'undefined' && data.targetData != null) {
-    let maxLineElement = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    maxLineElement.setAttribute("stroke-dasharray", "5,5");
-    maxLineElement.setAttribute("x1", "0");
-    maxLineElement.setAttribute("x2", "" + this.targetDims.maxW);
-    let maxLineHeight = (elemMaxValue - data.targetData) / elemMaxValue * this.targetDims.maxH;
-    maxLineElement.setAttribute("y1", "" + maxLineHeight);
-    maxLineElement.setAttribute("y2", "" + maxLineHeight);
-    maxLineElement.setAttribute("stroke", "#16a085");
-    barContainer.appendChild(maxLineElement);
-  }
-
-  //render axis (if user allows)
-  if(typeof data.axis === 'undefined' || data.axis) {
-    barContainer.appendChild(this.renderYAxis());
-    barContainer.appendChild(this.renderXAxis(data.ticks));
-  }
-
-  if(typeof data.ticks !== 'undefined') {
-    // TODO: render x and y axis
-    // this.renderYAxisTicks(barContainer, data.ticks, elemMaxValue);
-  }
-
-  //append container, which has at least all bars and any optional user inputs attached
-  anchor.appendChild(barContainer);
 };
 
-//TODO: getData() { returns data array, one element for each piece of data captured by Philepe (every time we init a new visual, push {container: data} to array) }
+const MockData = function (n, config) {
+
+  let maxNum = 25;
+  let arr = new Array();
+
+  /**
+   * Returns a random integer between two values min and max
+   * @param {int} min 
+   * @param {int} max
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
+   */
+  let getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  switch (config) {
+
+  case 'Linear':
+    for (let i = 0; i < n; i++) {
+      arr.push(getRandomInt(i, i + 10));
+    }
+    return arr;
+
+  default:
+    for (let i = 0; i < n; i++) {
+      arr.push(getRandomInt(0, maxNum));
+    }
+    return arr;
+  }
+};
