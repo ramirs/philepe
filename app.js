2c33d7ac95e32f8f00f98d305247b892bf99684c
ramirs <ramirs2015@gmail.com>
2018-10-04 10:38:45 -0700

    new dummy data

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 (function() {
 
-  let container = document.getElementById("bar-chart-container");
+  let mockData = new MockData(25, 'Linear');
+  let bulletChartTestData = [
+    {
+      min: 5,
+      max: 10,
+      val: 7
+    },
+    {
+      min: 12,
+      max: 15,
+      val: 6
+    },
+    {
+      min: 10,
+      max: 15,
+      val: 17
+    }
+  ];
 
-  new BarChart(container, new MockData(25, 'Linear'));
-  new LineChart(container, new MockData(25, 'Linear'));
-  new ScatterPlot(container, new MockData(25, 'Linear'));
+  new BarChart(document.getElementById("chart1"), mockData);
+  new LineChart(document.getElementById("chart2"), mockData);
+  new ScatterPlot(document.getElementById("chart3"), mockData);
+  new BulletChart(document.getElementById("chart4"), bulletChartTestData);
 
 })();
